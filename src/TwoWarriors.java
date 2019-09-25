f937322e070e22235bdfb69951f5f23bb51615d7
StreeenFour <diogodossantosmessi@gmail.com>
2019-09-25 09:18:54 -0300

    Modficação
    Apaguei a Interface Gráfica para colocar direto por códigos

--- src/TwoWarriors.java
+++ src/TwoWarriors.java
@@ -1,23 +1,19 @@
 import javax.swing.JFrame;
+
 public class TwoWarriors extends javax.swing.JFrame {
 
     public TwoWarriors() {
-
-       
-        setTitle("TwoWarriors"); //Titulo do game
-        //setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-        setSize(550, 450); //tamanho e largura da interface
+        
+        //add(new JButton("Teste"));
+        setTitle("TwoWarriors");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(550, 450);
         setLocationRelativeTo(null);//para tela aparecer no centro
         setResizable(false);//para o usuario não interferir na largura e altura do game
         setVisible(true);
-
     }
 
-    public static void main(String[] args) {        
-        ArenaInterface A = new ArenaInterface();
-        A.setVisible (true);
-        System.out.println("Olá");
-        
+    public static void main(String[] args) {
+        new TwoWarriors();
     }
-    
 }
