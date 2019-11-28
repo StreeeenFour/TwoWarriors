3b5f999d4ab88fb4b253a5c8b9abf298d85a6d5d
StreeenFour <diogodossantosmessi@gmail.com>
2019-11-28 17:59:15 -0300

    Add o menu
    Adicionei um menu e implementei a opção de sair do jogo.

--- src/TwoWarriors.java
+++ src/TwoWarriors.java
@@ -1,10 +1,51 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JSeparator;
 
 
 public class TwoWarriors extends JFrame {
 
     public TwoWarriors() {
         
+        JMenuBar barraMenu = new JMenuBar();
+		
+		JMenu menu = new JMenu("Menu");
+
+		JMenuItem sobre = new JMenuItem("Sobre");
+		sobre.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+
+				JOptionPane.showMessageDialog(null, "Jogo feito e criado por Diogo Dos Santos, Flávia dos Santos e Victória Termezana", "Informações", JOptionPane.INFORMATION_MESSAGE);
+			}
+
+            //@Override
+            //public void actionPerformed(ActionEvent ae) {
+              //  throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            //}
+		});
+		
+		JMenuItem sair = new JMenuItem("Sair");
+		sair.addActionListener(new ActionListener(){
+			
+			public void actionPerformed(ActionEvent e){
+				System.exit(0);
+			}
+		});
+		
+		menu.add(sobre);
+		menu.add(new JSeparator());
+		menu.add(sair);
+		
+		barraMenu.add(menu);
+		
+		setJMenuBar(barraMenu);
+		
         add(new Arena());
         //add(new JButton("Teste"));
         setTitle("TwoWarriors");
